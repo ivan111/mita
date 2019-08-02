4fafa57c2dcd6219df95543e571605d64c41bbe9
ivan111 <i@vanya.jp.net>
2019-08-03 04:34:53 +0900

    月日フィルターをjinjaに追加

--- jinja.py
+++ jinja.py
@@ -3,6 +3,11 @@ from flask import request, url_for
 
 def use_jinja(app):
 
+    @app.template_filter()
+    def ym(num):
+        text = str(num)
+        return '{}-{}'.format(text[:4], text[4:])
+
     def url_for_other_page(page):
         args = request.view_args.copy()
         args['page'] = page
